b62c2d23067642b300189fb4114e74c093cf782c
MaximPuga <62074235+MaximPuga@users.noreply.github.com>
2023-04-27 02:17:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,52 +10,71 @@ Test data for 6.: First use 'Mingueza', 'Messi', 'Modrich' and 'Nacho' players. 
 
 */
 
+/* TASK2
+Continue using the previously used game variable.
+1. Loop through the game.scored array and print the name of each player to the console along with the goal number (example: “Goal 1 - Kroos”)
+2. Use a loop to calculate the average odd and print it to the console 
+3. Print 3 odds to the console, but formatted, exactly like this:
+Rate for REAL MADRID victory: 1.48 
+Rate for draw: 2.53
+Rate for BARCELONA victory: 4.25
+Get team names directly from the game object, don't hardcode them 
+4. Bonus: create an object called goalScorers that contains the names of the players who scored as properties and the number of goals as a value. In this game it will look like this:
+      {
+        Kroos: 1,
+        Benzema: 1,
+        Mingueza: 1
+}
+
+*/
+
+//1
 const game = {
-   team1: 'REAL MADRID',
-   team2: 'BARCELONA',
-   players: [
-     [
-       'Courtois',
-       'Vazquez',
-       'Militao',
-       'Nacho',
-       'Mendy',
-       'Casemiro',
-       'Valverde',
-       'Modrich',
-       'Kroos',
-       'Vinicius',
-       'Benzema',
-     ],
-     [
-       'Stegen',
-       'Mingueza',
-       'Araujo',
-       'Lenglet',
-       'Dest',
-       'Busquets',
-       'Jong',
-       'Alba',
-       'Messi',
-       'Pedri',
-       'Dembele',
-     ],
-   ],
-   score: '2:1',
-   scored: ['Kroos', 'Benzema', 'Mingueza'],
-   date: 'Apr 10th, 2021',
-   odds: {
-     team1: 1.48,
-     draw: 2.53,
-     team2: 4.25,
-   },
-  };
+  team1: 'REAL MADRID',
+  team2: 'BARCELONA',
+  players: [
+    [
+      'Courtois',
+      'Vazquez',
+      'Militao',
+      'Nacho',
+      'Mendy',
+      'Casemiro',
+      'Valverde',
+      'Modrich',
+      'Kroos',
+      'Vinicius',
+      'Benzema',
+    ],
+    [
+      'Stegen',
+      'Mingueza',
+      'Araujo',
+      'Lenglet',
+      'Dest',
+      'Busquets',
+      'Jong',
+      'Alba',
+      'Messi',
+      'Pedri',
+      'Dembele',
+    ],
+  ],
+  score: '2:1',
+  scored: ['Kroos', 'Benzema', 'Mingueza'],
+  date: 'Apr 10th, 2021',
+  odds: {
+    team1: 1.48,
+    draw: 2.53,
+    team2: 4.25,
+  },
+ };
 
-  const [players1, players2] = game.players;
-  console.log(players1, players2);
+ const [players1, players2] = game.players;
+ console.log(players1, players2);
 
-  const [goalkeeper, ...fieldPlayers] = players1;
-  console.log(goalkeeper, fieldPlayers);
+ const [goalkeeper, ...fieldPlayers] = players1;
+ console.log(goalkeeper, fieldPlayers);
 
 const allPlayers = [...players1, ...players2];
 console.log(allPlayers);
@@ -64,15 +83,46 @@ const players1Total = [...players1, 'Marcelo', 'Isco', 'Asensio', 'Diaz', 'Odrio
 console.log(players1Total);
 
 const {
-   odds: {team1, draw, team2},
+  odds: {team1, draw, team2},
 } = game;
 console.log(team1, draw, team2);
 
 const printGoals = function (...gamePlayers)  {
-   console.log(`${gamePlayers.length} goals scored`);
+  console.log(`${gamePlayers.length} goals scored`);
 };
 
 
 printGoals(...game.scored);
 
 team1 < team2 && console.log(`${game.team1} is more likely to win`);
+
+//2
+
+for (const entry of game.scored.entries()) {
+ console.log(`Goal ${entry[0] + 1} - ${entry[1]}`);
+};
+
+let oddSum = 0;
+let odds = Object.values(game.odds);
+
+for(const odd of odds) {
+ oddSum += odd;
+};
+
+const avgOdd = oddSum / odds.length;
+console.log(avgOdd);
+
+for(const [teamName, odd] of Object.entries(game.odds)) {
+ const switchTxt =
+  teamName === 'draw' ? 'draw' : `${game[teamName]} victory`;
+ console.log(`Rate for ${switchTxt}: ${odd}`);
+};
+
+const goalScores = {};
+
+for(const player of game.scored) {
+ goalScores[player] = 1;
+};
+
+console.log(goalScores);
+
